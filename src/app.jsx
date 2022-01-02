ed62f452d1caa5ef3f8b13f02b6a79e54330007b
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2022-01-02 15:58:46 +0100

    display most popular youtube video titles

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,30 @@
 import './app.css';
+import React, { useEffect, useState } from 'react';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>hello</h1>;
+  const [videos, setvideos] = useState([]);
+  useEffect(() => {
+    console.log('useEffect');
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD73d7qyOMwjG3v0L2WiockHPZbXjqOQFk',
+      requestOptions
+    )
+      .then((response) => response.json())
+      // .then((result) => console.log('result', result.items))
+      .then((result) => setvideos(result.items)) //update videos
+      .catch((error) => console.log('error', error));
+  }, []);
+  return (
+    <h1>
+      <VideoList videos={videos} />
+    </h1>
+  );
 }
 
 export default App;
