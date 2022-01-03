b099949575b2b99f4380472991b6564ea685efbe
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2022-01-03 20:53:56 +0100

    video image and title list added

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+const VideoItem = (props) => (
+  <li>
+    <img
+      src={props.video.snippet.thumbnails.medium.url}
+      alt="video thumbnail"
+    />
+    <div>
+      <p>{props.video.snippet.title}</p>
+      <p>{props.video.snippet.channelTitle}</p>
+    </div>
+  </li>
+);
 
 export default VideoItem;
