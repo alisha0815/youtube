926deba4b48ca305858813370b395ae2f9120b32
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2022-01-04 13:13:44 +0100

    refactoring for better networking

--- src/app.jsx
+++ src/app.jsx
@@ -3,40 +3,45 @@ import React, { useEffect, useState } from 'react';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
 
-function App() {
+function App({ youtube }) {
   const [videos, setvideos] = useState([]);
   const search = (query) => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&masResults=25&q=${query}&type=video&key=AIzaSyD73d7qyOMwjG3v0L2WiockHPZbXjqOQFk`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setvideos(items))
-      .catch((error) => console.log('error', error));
+    youtube
+      .search(query) //
+      // .then(console.log);
+      .then((videos) => setvideos(videos));
+    // const requestOptions = {
+    //   method: 'GET',
+    //   redirect: 'follow',
+    // };
+    // fetch(
+    //   `https://youtube.googleapis.com/youtube/v3/search?part=snippet&masResults=25&q=${query}&type=video&key=AIzaSyD73d7qyOMwjG3v0L2WiockHPZbXjqOQFk`,
+    //   requestOptions
+    // )
+    //   .then((response) => response.json())
+    //   .then((result) =>
+    //     result.items.map((item) => ({ ...item, id: item.id.videoId }))
+    //   )
+    //   .then((items) => setvideos(items))
+    //   .catch((error) => console.log('error', error));
   };
   useEffect(() => {
-    console.log('useEffect');
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD73d7qyOMwjG3v0L2WiockHPZbXjqOQFk',
-      requestOptions
-    )
-      .then((response) => response.json())
-      // .then((result) => console.log('result', result.items))
-      .then((result) => setvideos(result.items)) //update videos
-      .catch((error) => console.log('error', error));
+    youtube
+      .mostPopular() //
+      // .then(console.log);
+      .then((videos) => setvideos(videos));
+    // const requestOptions = {
+    //   method: 'GET',
+    //   redirect: 'follow',
+    // };
+    // fetch(
+    //   'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD73d7qyOMwjG3v0L2WiockHPZbXjqOQFk',
+    //   requestOptions
+    // )
+    //   .then((response) => response.json())
+    //   // .then((result) => console.log('result', result.items))
+    //   .then((result) => setvideos(result.items)) //update videos
+    //     .catch((error) => console.log('error', error));
   }, []);
   return (
     <div className={styles.app}>
