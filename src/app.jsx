0dbe1654a38b5b836ced2acdbad5b630899dbe49
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2022-01-04 09:52:58 +0100

    search component added

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
-import './app.css';
+import styles from './app.module.css';
 import React, { useEffect, useState } from 'react';
 import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 
 function App() {
   const [videos, setvideos] = useState([]);
@@ -21,9 +22,10 @@ function App() {
       .catch((error) => console.log('error', error));
   }, []);
   return (
-    <h1>
+    <div className={styles.app}>
+      <SearchHeader />
       <VideoList videos={videos} />
-    </h1>
+    </div>
   );
 }
 
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,18 @@
+import styles from './search_header.module.css';
+
+import React from 'react';
+
+const SearchHeader = (props) => {
+  return (
+    <header>
+      <img src="/images/logo.png" alt="logo" />
+      <h1>Youtube</h1>
+      <input type="search" placeholder="Search..." />
+      <button type="submit">
+        <img src="/images/search.png" alt="search" />
+      </button>
+    </header>
+  );
+};
+
+export default SearchHeader;
